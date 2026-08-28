6c3a56e0997a7d77dab1ba1a08b3d212e249375b
LeoVegaSys <lionel_almeida@vegayan.com>
2026-08-28 17:40:20 +0530

    update mcp config

--- config/mcp.py
+++ config/mcp.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
 ### MCP CONFIG
-MCP_DB_PATH=""
 MCP_DB_URL="http://127.0.0.1:8080/mcp"
+MCP_HOST="127.0.0.1"
+MCP_PORT="8080"
 # MCP_CONFIG= {
 #             # "time": {
 #             #     "transport": "stdio",
@@ -21,28 +22,3 @@ MCP_DB_URL="http://127.0.0.1:8080/mcp"
 #             }
 #         }
 
-MCP_CONFIG={
-    "mysql":{
-        "server":{
-                "transport": "http",  # HTTP-based remote server
-                # Ensure you start your weather server on port 8000
-                "url": "http://127.0.0.1:8080/mcp",
-            },
-        "query_function": "run_query",
-        "query_key": "query",
-    },
-    "duckdb":{
-        "server": {
-                "command": "uvx",
-                "args": ["mcp-server-motherduck", "--db-path", "./dump/duckdb/traffic_data_last_3days.db"],
-                         #"--max-rows", "10000", "--max-chars", "1000000"],
-                         #"--max-rows", "100000", "--max-chars", "10000000"],
-                "transport": "stdio"
-            },
-        "query_function": "execute_query",
-        "query_key": "sql",
-    },
-}
-
-#Valid values : mysql, duckdb, oracle, redis, postgresql
-MCP_DB_TYPE="duckdb" 
--- db/vegayan_mpls/mcp.py
+++ db/vegayan_mpls/mcp.py
@@ -1,13 +1,14 @@
 from db.vegayan_mpls.config import TRAFFIC_DB_NAME, MCP_DB_TYPE
+from config.mcp import MCP_HOST, MCP_PORT
 
 MCP_CONFIG={
     MCP_DB_TYPE:{
         "server":{
                 "transport": "http",  # HTTP-based remote server
                 # Ensure you start your weather server on port 8000
-                "url": "http://127.0.0.1:8080/mcp",
+                "url": f"http://{MCP_HOST}:{MCP_PORT}/mcp",
             },
-        "query_function": "run_query",
+        "query_function": "arun_query",
         "query_key": "query",
         "query_db": TRAFFIC_DB_NAME,
     }
